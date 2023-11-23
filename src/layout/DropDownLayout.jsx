bea025f03e979dcc9f888aa2d5a2e43e96c84076
Devesh Bisht <108985813+dev0869@users.noreply.github.com>
2023-11-23 14:58:37 +0530

    Create DropDownLayout.jsx
    layout done

---
+++ src/layout/DropDownLayout.jsx
@@ -0,0 +1,10 @@
+<div className="relative border-red-500 border-2 m-4 h-[90vh]">
+  <div className="relative border-red-500 border-2 m-4 h-[35vh]">
+    <p>asad</p>
+
+{/*     <div className="absolute bottom-0 bg-red-700 border-red-500 border-2 transform translate-y-full h-[20vh]">
+      <p>asad</p>
+      <p>asdadssad</p> */} thease lines are imp
+    </div>
+  </div>
+</div>
